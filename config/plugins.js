bde742e3f6c1dfcba2272629fd2ef4de4fa5daa7
kumatsegayie <kumatsegae@gmail.com>
2026-06-03 03:30:03 +0300

    No reply fixed #2

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   email: {
     config: {
       settings: {
-        defaultFrom: env("SMTP_FROM", '"Litloom" <no-reply@litloomapp.com>'),
+        defaultFrom: env("SMTP_FROM", '"Litloom" <support@litloomapp.com>'),
         defaultReplyTo: env("SMTP_REPLY_TO", env("SMTP_FROM", "support@litloomapp.com"))
       }
     }
